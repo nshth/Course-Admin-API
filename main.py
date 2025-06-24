41b9391c5e3d65d63687a90d9b4c1e3328283fd1
Nashath Rasha <nashathrasha0@gmail.com>
2025-06-25 01:42:24 +0530

    retrieve the relevant course information sorted

--- main.py
+++ main.py
@@ -13,11 +13,32 @@ courses = db['courses']
 @app.get('/courses')
 def get_course(sort_by: str = 'date', domain: str = None):
     # set the rating.total and rating.count to all the courses based on the sum of the chapters rating
-    for course in courses['courses']:
+    for course in courses.find():
         total = 0
         count = 0
         for chapter in course['chapters']:
             with contextlib.suppress(KeyError):
                 total += chapter['rating']['total']
-                count += total + chapter['rating']['count']
+                count += chapter['rating']['count']
         courses.update_one({'_id': course['_id']}, {'$set': {'rating': {'total':total, 'count':count}}})
+
+    # sort_by == 'date' [DESCENDING]
+    if sort_by == 'date':
+        sort_field = 'date'
+        direction = -1
+    # sort_by == 'rating' [DESCENDING]
+    if sort_by == 'rating':
+        sort_field = 'rating'
+        direction = -1
+# sort_by == 'alphabetical' [ASCENDING]
+    if sort_by == 'alphabetical':
+        sort_field = 'name'
+        direction = 1
+# set query
+    query = {}
+    if domain:
+        query['domain'] = domain
+# queries the MongoDB database to retrieve the relevant course information
+    course = courses.find(query, {'chapters': 0, '_id': 0}).sort(sort_field, direction)
+# return the courses in a list
+    return list(courses)
